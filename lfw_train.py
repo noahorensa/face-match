d984c6d11eeade1e72420d5e3df1b82b24a04528
Noah Orensa <noahorensa@gmail.com>
2020-04-05 17:42:45 -0600

    train on gpu

--- lfw_train.py
+++ lfw_train.py
@@ -1,29 +1,32 @@
+import tensorflow as tf
+import lfw
+import model
+
 IMAGE_SHAPE = (72, 72, 3)
 BATCH_SIZE = 32
 NUM_EPOCHS = 20
 
 
 def main():
-  import lfw
 
-  # prepare the model
-  from model import create_model
-  model = create_model(
-    shape=IMAGE_SHAPE,
-    num_classes=lfw.NUM_CLASSES
-  )
+  with tf.device('/gpu:0'):
+    # prepare the model
+    m = model.create_model(
+      shape=IMAGE_SHAPE,
+      num_classes=lfw.NUM_CLASSES
+    )
 
-  # prepare the dataset
-  train_ds = lfw.read_dataset()
-  print(train_ds)
-  train_ds = train_ds.batch(BATCH_SIZE, drop_remainder=True).repeat()
+    # prepare the dataset
+    train_ds = lfw.read_dataset()
+    print(train_ds)
+    train_ds = train_ds.batch(BATCH_SIZE, drop_remainder=True).repeat()
 
-  # train the model
-  model.fit(
-    train_ds,
-    epochs=NUM_EPOCHS,
-    steps_per_epoch=lfw.NUM_EXAMPLES / BATCH_SIZE
-  )
+    # train the model
+    m.fit(
+      train_ds,
+      epochs=NUM_EPOCHS,
+      steps_per_epoch=lfw.NUM_EXAMPLES / BATCH_SIZE
+    )
 
 
 if __name__ == "__main__":
